1efb499cb7c960b9adaf56f98755374eccbdd708
lucifiere <lucifiere@126.com>
2018-11-03 15:38:28 +0800

    规范代码

--- src/main/groovy/com/lucfiere/file/DefaultFileHelper.groovy
+++ src/main/groovy/com/lucfiere/file/DefaultFileHelper.groovy
@@ -4,14 +4,11 @@ import java.lang.reflect.Field
 
 class DefaultFileHelper implements FileHelper {
 
-    private static Map<String,String> outputInfo
-
-    static {
-        outputInfo = new HashMap<>()
-        outputInfo.put("pojoContent","Pojo.java")
-        outputInfo.put("daoContent","Dao.java")
-        outputInfo.put("mapperContent","Mapper.java")
-    }
+    private static Map<String, String> OUTPUT_INFO = [
+            "pojoContent"  : "Pojo.java",
+            "daoContent"   : "Dao.java",
+            "mapperContent": "Mapper.java"
+    ]
 
     @Override
     String loadDdlFile(String path) {
@@ -19,54 +16,46 @@ class DefaultFileHelper implements FileHelper {
     }
 
     private static String loadFile(String path) {
-        try {
-            StringBuffer sb = new StringBuffer()
-            BufferedReader bf = new BufferedReader(new FileReader(path))
-            String s
-            while ((s = bf.readLine()) != null) {
-                sb.append(s).append("\n")
-            }
-            System.out.print(sb.toString())
-            sb.toString()
-        } catch (FileNotFoundException e) {
-            e.printStackTrace()
+        StringBuffer sb = new StringBuffer()
+        BufferedReader bf = new BufferedReader(new FileReader(path))
+        String s
+        while ((s = bf.readLine()) != null) {
+            sb.append(s).append("\n")
         }
+        sb.toString()
     }
 
     @Override
     void exportSourceCodeFile(SourceCodeBundle sourceCodeBundle, String targetPath) {
-        Map<String,String> content = getOutPutContent(sourceCodeBundle)
+        Map<String, String> content = getOutPutContent(sourceCodeBundle)
         content.each {
-            create(targetPath,sourceCodeBundle.entityName,it.key,it.value)
+            create(targetPath, sourceCodeBundle.entityName, it.key, it.value)
         }
     }
 
-    private static Map<String,String> getOutPutContent(SourceCodeBundle sourceCodeBundle) {
+    private static Map<String, String> getOutPutContent(SourceCodeBundle sourceCodeBundle) {
         Field[] fields = sourceCodeBundle.getClass().getDeclaredFields()
-        Map result = new HashMap()
-        for (def i = 0;i<fields.size();i++) {
-            if (outputInfo.keySet().contains(fields[i].getName())) {
-                result.put( sourceCodeBundle.getProperty(fields[i].getName()),outputInfo.get(fields[i].getName()))
+        Map result = [:]
+        for (def i = 0; i < fields.size(); i++) {
+            if (OUTPUT_INFO.keySet().contains(fields[i].getName())) {
+                result.put(sourceCodeBundle.getProperty(fields[i].getName()), OUTPUT_INFO.get(fields[i].getName()))
             }
         }
         result
     }
 
-    private static void create(String path,String name,String resource,String suffix) {
+    private static void create(String path, String name, String resource, String suffix) {
         File file = new File(path + File.separator + name)
         if (!file.exists()) {
             file.mkdir()
         }
-        File nextFile = new File(file,name+suffix)
+        File nextFile = new File(file, name + suffix)
         if (!nextFile.exists()) {
             nextFile.createNewFile()
         }
-        try {
-            FileOutputStream outputStream = new FileOutputStream(nextFile)
-            outputStream.write(resource.getBytes())
-            outputStream.close()
-        } catch (Exception e) {
-            e.printStackTrace()
-        }
+        FileOutputStream outputStream = new FileOutputStream(nextFile)
+        outputStream.write(resource.getBytes())
+        outputStream.close()
     }
+
 }
